34d5558332dfb3c141f11d0c6e29024fc38e5f61
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-08-26 10:09:52 +0100

    Create styles.js

---
+++ components/Overview/styles.js
@@ -0,0 +1,71 @@
+import { Box, InputBase } from "@mui/material";
+import { alpha, styled } from "@mui/material/styles";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.1),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.black, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+const LocationIcon = styled(Box)(({ theme }) => ({
+  width: 37,
+  height: 38,
+  border: "1px solid rgb(19, 47, 76)",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.1),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.black, 0.25),
+  },
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const ThemeIcon = styled(Box)(({ theme }) => ({
+  width: 37,
+  height: 38,
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.1),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.black, 0.25),
+  },
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+export { SearchIconWrapper, StyledInputBase, Search, LocationIcon, ThemeIcon };

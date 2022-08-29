b403a31f8b9b2e3868922069347bb0079640b7bc
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-08-29 09:55:35 +0100

    Update CardWidgets.jsx

--- components/Widgets/CardWidgets.jsx
+++ components/Widgets/CardWidgets.jsx
@@ -3,25 +3,34 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import { FlexAlignCenter, FlexSpaceBtw } from "../../styles/globalStyles";
 
-const card = (
-  <React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Word of the Day
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">well meaning and kindly.</Typography>
-    </CardContent>
-  </React.Fragment>
-);
-
-const CardWidgets = () => {
+const CardWidgets = ({ icon, title, weather }) => {
   return (
     <Box>
-      <Card variant="outlined">{card}</Card>
+      <Card variant="outlined">
+        <CardContent>
+          <FlexSpaceBtw>
+            <FlexAlignCenter>
+              <Box sx={{ mr: 3 }}>{icon}</Box>
+              <Box>
+                <Typography
+                  variant="subtitle2"
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  {title}
+                </Typography>
+                <Typography variant="h5" fontWeight={600}>
+                  {weather}
+                </Typography>
+              </Box>
+            </FlexAlignCenter>
+
+            <Typography>Time</Typography>
+          </FlexSpaceBtw>
+        </CardContent>
+      </Card>
     </Box>
   );
 };

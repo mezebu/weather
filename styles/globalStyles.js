933884a3a8856e6d5daade1426f7209d6b0a2327
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-08-30 14:14:34 +0100

    Create globalStyles.js

---
+++ styles/globalStyles.js
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const FlexCenter = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+export const FlexSpaceBtw = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
+
+export const FlexAlignCenter = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+});

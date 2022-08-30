503f61f5ff9f082c799a59abfe2a40a79ec23ea0
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-08-30 14:14:37 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Box from "@mui/material/Box";
+import { Grid, Box } from "@mui/material";
+
 import Head from "next/head";
-import { Button, Grid } from "@mui/material";
 import Overview from "../components/Overview/Overview";
 import Widgets from "../components/Widgets/Widgets";
 
+import useOpenWeather from "../api/useOpenWeather";
+
 export default function Index() {
+  const { data, loading } = useOpenWeather(
+    `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`
+  );
+
   return (
     <Box>
       <Head>
@@ -15,10 +21,10 @@ export default function Index() {
       <Box component="main">
         <Grid container spacing={2}>
           <Grid item lg={8} md={8} sm={12} xs={12}>
-            <Overview />
+            <Overview data={data} isLoading={loading} />
           </Grid>
           <Grid item lg={4} md={4} sm={12} xs={12}>
-            <Widgets />
+            <Widgets data={data} />
           </Grid>
         </Grid>
       </Box>

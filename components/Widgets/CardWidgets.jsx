3cd8e5f1eff91ebbd048f624493072ee3eb9adf0
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-01 10:39:31 +0100

    Update CardWidgets.jsx

--- components/Widgets/CardWidgets.jsx
+++ components/Widgets/CardWidgets.jsx
@@ -4,10 +4,11 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { FlexAlignCenter, FlexSpaceBtw } from "../../styles/globalStyles";
-import moment from "moment";
+import moment from "moment-timezone";
 
 const CardWidgets = ({ icon, title, weather }) => {
   const dateTime = moment.unix(weather).format("LT");
+
   return (
     <Box>
       <Card variant="outlined">

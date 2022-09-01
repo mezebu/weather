0e870ffc3e8d37c04890fd654792915a272f4b37
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-01 10:39:37 +0100

    Update MinMaxTemp.jsx

--- components/Overview/CardWidgets/MinMaxTemp.jsx
+++ components/Overview/CardWidgets/MinMaxTemp.jsx
@@ -5,7 +5,7 @@ import { grey } from "@mui/material/colors";
 import AcUnitRoundedIcon from "@mui/icons-material/AcUnitRounded";
 import DeviceThermostatRoundedIcon from "@mui/icons-material/DeviceThermostatRounded";
 
-const MinMaxTemp = ({ value, title, isLoading }) => {
+const MinMaxTemp = ({ value, title }) => {
   const icon =
     value >= 20 ? (
       <DeviceThermostatRoundedIcon color="primary" fontSize="large" />
@@ -15,6 +15,7 @@ const MinMaxTemp = ({ value, title, isLoading }) => {
   return (
     <Card
       elevation={0}
+      variant="outlined"
       sx={{
         "&:hover": { background: grey[100] },
         backgroundColor: grey[50],
@@ -28,36 +29,20 @@ const MinMaxTemp = ({ value, title, isLoading }) => {
             alignItems: "center",
           }}
         >
-          {isLoading ? (
-            <Skeleton
-              animation="wave"
-              variant="circular"
-              width={40}
-              height={40}
-            />
-          ) : (
-            <Box>{icon}</Box>
-          )}
+          <Box>{icon}</Box>
 
           <Box sx={{ ml: 5 }}>
-            {isLoading ? (
-              <Stack spacing={2}>
-                <Skeleton variant="text" width={90} />
-                <Skeleton variant="text" />
-              </Stack>
-            ) : (
-              <Stack>
-                <Typography color="text.secondary" variant="subtitle2">
-                  {title}
+            <Stack>
+              <Typography color="text.secondary" variant="subtitle2">
+                {title}
+              </Typography>
+              <Typography variant="h5" fontWeight={600}>
+                {Math.round(value)}
+                <Typography component="span" fontWeight={500}>
+                  &deg;C
                 </Typography>
-                <Typography variant="h5" fontWeight={600}>
-                  {Math.round(value)}
-                  <Typography component="span" fontWeight={500}>
-                    &deg;C
-                  </Typography>
-                </Typography>
-              </Stack>
-            )}
+              </Typography>
+            </Stack>
           </Box>
         </Box>
       </CardContent>

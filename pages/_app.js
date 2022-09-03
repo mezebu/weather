cdb778775555c0539455763385031636b217ffe3
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-03 11:10:04 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
 import theme from "../src/theme";
 import createEmotionCache from "../src/createEmotionCache";
+import { useMediaQuery } from "@mui/material";
+import ColorModeContext from "../src/ColorModeContext";
+import darkTheme from "../src/darkTheme";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -12,13 +16,35 @@ const clientSideEmotionCache = createEmotionCache();
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
+  // Set dark mode based on media query
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [darkMode, setDarkMode] = useState(prefersDarkMode);
+
+  useEffect(() => {
+    const mode = localStorage.getItem("mode") === "true";
+    // set mode
+    console.log(`get localStore ${mode}`);
+    setDarkMode(mode);
+  }, []);
+
+  const _setDarkMode = (newmode) => {
+    console.log(`set localStore ${newmode}`);
+    localStorage.setItem("mode", newmode);
+    setDarkMode(newmode);
+  };
+
   return (
     <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      {/* <ColorModeContext.Provider value={colorMode}> */}
+      <ColorModeContext.Provider
+        value={{ darkMode, setDarkMode: _setDarkMode }}
+      >
+        <ThemeProvider theme={darkMode ? darkTheme : theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ColorModeContext.Provider>
     </CacheProvider>
   );
 }

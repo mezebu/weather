8a865e7043ed6903fd1b388a43e36c0c06c95de8
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-03 11:09:56 +0100

    Create darkTheme.js

---
+++ src/darkTheme.js
@@ -0,0 +1,23 @@
+import { red } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#1F1B24",
+      paper: "#332940",
+    },
+  },
+});
+
+export default darkTheme;

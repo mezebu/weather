515a4e0daca10363e11a3ac067e993e1292c5809
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-04 08:25:39 +0100

    Create Visibility.jsx

---
+++ components/Overview/CardWidgets/Visibility.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Box, CardContent, Typography, Stack } from "@mui/material";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import PropTypes from "prop-types";
+
+import { OverviewCard } from "../styles";
+
+const Visibility = ({ value }) => {
+  const visibility = value / 1000;
+
+  return (
+    <OverviewCard elevation={0} variant="outlined">
+      <CardContent>
+        <Box
+          sx={{
+            p: 1,
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <VisibilityOutlinedIcon fontSize="large" />
+
+          <Box sx={{ ml: 5 }}>
+            <Stack>
+              <Typography color="text.secondary" variant="subtitle2">
+                Visibility
+              </Typography>
+              <Typography variant="h5" fontWeight={600}>
+                {visibility}
+                <Typography component="span" fontWeight={500}>
+                  {" "}
+                  Km
+                </Typography>
+              </Typography>
+            </Stack>
+          </Box>
+        </Box>
+      </CardContent>
+    </OverviewCard>
+  );
+};
+
+export default Visibility;
+
+Visibility.propTypes = {
+  value: PropTypes.number,
+};
